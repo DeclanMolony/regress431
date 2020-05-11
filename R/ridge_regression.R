f2ba92fb98cc89210c664b4cb6af44709c870f15
DeclanMolony <declanmolony@yahoo.com>
2020-05-10 20:17:24 -0700

    Update ridge_regression.R

--- R/ridge_regression.R
+++ R/ridge_regression.R
@@ -54,6 +54,31 @@ ridge_regression <- function(dat, response, lambda) {
 
 }
 
+#' asdfsdf
+#'
+#' @param
+#'
+find_best_lambda_helper <- function(x,y,y_test,lambda) {
+
+  coefs <- as.data.frame(t(solve(t(x) %*% x + lambda*diag(ncol(x))) %*% (t(x) %*% y)))
+
+  coefs2 <- purrr::map_dfr(1:nrow(x), ~coefs[1,]) #repeat the coefficients the number of rows in x
+
+  combined <- x * coefs2 #multiply the explanatory variables by their corresponding coefficients
+  combined <- data.frame(rowSums(combined)) #find the row sums, or predicted values of each row
+  combined <- cbind(y_test,combined)
+  combined <- combined %>%
+    dplyr::rename("predicted_value" = rowSums.combined.)
+  combined <- combined %>%
+    mutate(diff = ((y_test - predicted_value)^2)) %>%
+    summarise(error = sum(diff))
+
+  combined <- as.data.frame(cbind(lambda,combined))
+
+  return(combined)
+
+}
+
 #' Determines the best penalty term from a set of options
 #'
 #' This function uses a randomly chosen test and training set
@@ -73,11 +98,18 @@ ridge_regression <- function(dat, response, lambda) {
 #' @export
 find_best_lambda <- function(train_dat, test_dat, response, lambda) {
 
+  x <- train_dat %>% dplyr::select(-{{response}})
+  y <- as.matrix(train_dat %>% dplyr::pull({{response}}))
+  y_test <- as.matrix(test_dat %>% dplyr::pull({{response}}))
+
+  x <- scale(x)
+  x <- as.matrix(cbind(1, x))
+
 
   ### lambda_errors should be a data frame with two columns: "lambda" and "error"
   ### For each lambda, you should record the resulting Sum of Squared error
   ### (i.e., the predicted value minus the real value squared) from prediction
-  ### on the test dataset.
+  ### on the test dataset. ie: sum((y-hat - yi)^2)
 
   return(lambda_errors)
 }
